4e1dc46e0e0b93bc6b233bc828005c3bd7fa7e53
90537 <90537@CACTUSBOOK>
2024-04-02 22:31:22 +0300

    Add project files.

---
+++ MusteriManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        List<Musteri> _musteriList;
+
+        public MusteriManager(List<Musteri> musteriList)
+        {
+            _musteriList=musteriList;
+        }
+
+        public MusteriManager()
+        {
+        }
+
+        public void Add(List<Musteri>musteriler)
+        { 
+            foreach (Musteri m in musteriler)
+            {
+                Console.WriteLine(m.MusteriAd+": İsimli Müşteri Başarılı Bir Şekilde Eklenmiştir.");
+            }
+        }
+        public void List(List<Musteri> musteriler)
+        {
+            foreach (Musteri m in musteriler)
+            {                
+                Console.WriteLine("Id : "+m.MusteriId);
+                Console.WriteLine("İsim :"+m.MusteriAd);
+                Console.WriteLine("Soyisim : "+m.MusteriSoyad);
+            }
+        }
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Başarıyla Silinmiştir!");
+            //Musteri musteriToDelete = _musteriList.SingleOrDefault(md => md.MusteriId==musteri.MusteriId);
+            //_musteriList.Remove(musteriToDelete);
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Musteri>musteriler = new List<Musteri>();
+            Musteri musteri1 = new Musteri {MusteriAd="Enes",MusteriSoyad="Sarıhan",MusteriId=1 };
+            Musteri musteri2 = new Musteri {MusteriAd="Engin",MusteriSoyad="Demiroğ",MusteriId=2 };
+            Musteri musteri3 = new Musteri {MusteriAd="Johnny",MusteriSoyad="Depp",MusteriId=3 };
+
+            musteriler.Add(musteri1);
+            musteriler.Add(musteri2);
+            musteriler.Add(musteri3);
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteriler);
+
+            musteriManager.List(musteriler);
+
+            musteriManager.Delete(musteri3);
+            
+            
+
+            Console.ReadKey();
+        }
+    }
+}
